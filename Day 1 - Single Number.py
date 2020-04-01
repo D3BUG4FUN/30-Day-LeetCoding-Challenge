a9cb629500cbdf3e2c1e78fef6d06328e9176baf
D3BUG4FUN <D3BUG4FUN@protonmail.com>
2020-04-01 15:50:55 +0800

    Create Day 1 - Single Number.py

---
+++ Day 1 - Single Number.py
@@ -0,0 +1,10 @@
+# -*- encoding: UTF-8 -*-
+
+
+from functools import reduce
+from typing import List
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        return reduce(lambda x, y: x ^ y, nums)
